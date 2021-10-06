331e95ff7b642a5380517967fb3cfc8e8ed3ef18
adoronin158@gmail.com <adoronin158@gmail.com>
2021-10-06 20:14:29 +0300

    2123

---
+++ 2.10.2021/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._10._2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int userInput;
+            bool isWork = true;
+            Restourane restourane = new Restourane(10, 10,0);
+            while (isWork==true)
+            {
+                Console.WriteLine();
+
+
+                Console.WriteLine("Введите 1 если хотите посмотреть сколько доступно столов");
+                Console.WriteLine("Введите 2 если хотите добавить бронь");
+                Console.WriteLine("Введите 3 если хотите удалить бронь");
+                Console.WriteLine("Введите 4 если хотите выйти");
+                userInput = int.Parse(Console.ReadLine());
+                switch (userInput)
+                {
+                    case 1:
+                        Console.WriteLine();
+                        restourane.CheckTables(restourane);
+                        break;
+                    case 2:
+                        restourane.AddReservation(restourane);
+                        break;
+                    case 3:
+                        restourane.DeleteReservation(restourane);
+                        break;
+                    case 4:
+                        isWork = false;
+                        break;
+                    default:
+                        Console.WriteLine("Ошибка введите корректное число");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ 2.10.2021/Restourane.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2._10._2021
+{
+    class Restourane
+    {
+        private int _tables;
+        private int _clearTables;
+      
+        private int _busyTables;
+
+        public Restourane(int tables, int clearTables, int busyTables)
+        {
+            _tables = tables;
+            _clearTables = clearTables;
+            _busyTables = busyTables;
+            
+        }
+
+        public void CheckTables(Restourane restourane)
+        {
+            Console.WriteLine($"{ restourane._tables} <-----всего столов ");
+            Console.WriteLine($"{restourane._clearTables} <------всего свободно столов");
+            Console.WriteLine($"{restourane._busyTables} <----- всего забронировано");
+        }
+
+        public void AddReservation(Restourane restourane)
+        {
+            if (_clearTables == 0)
+            {
+                Console.WriteLine("ОШИБКА СВОБОДНЫЕ СТОЛЫ ЗАКОНЧИЛИСЬ");
+            }
+            else if (_clearTables > 0)
+            {
+                _clearTables = _clearTables - 1;
+                _busyTables = _busyTables + 1;
+            }
+        }
+
+        public void DeleteReservation(Restourane restourane)
+        {
+            if (_busyTables == 0)
+            {
+                
+                Console.WriteLine("ОШИБКА ЗАНЯТЫХ СТОЛОВ НЕТ");
+            }
+            else if (_busyTables >= 0)
+            {
+                _clearTables = _clearTables + 1;
+                _busyTables = _busyTables - 1;
+            }
+        }
+
+        
+
+    }
+}
